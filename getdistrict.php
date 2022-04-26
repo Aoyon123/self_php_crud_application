fdd24afa4231556f06f43df750fde1cf3ff52d70
MD FAHAMIDUL HAQUE <49103044+Aoyon123@users.noreply.github.com>
2022-04-26 15:49:45 +0600

    Add files via upload

--- getdistrict.php
+++ getdistrict.php
@@ -1,17 +1,22 @@
 <?php
 include 'connection.php';
 
-$id=$_GET['id'];
-$sql="SELECT * FROM district2 WHERE division-id = $id";
+$id = $_GET['id'];
+//echo($id);exit;
+//echo($_GET['id']);exit;
+$sql = "SELECT * FROM district WHERE division_id = $id";
 
-$result=mysqli_query($conn,$sql);
+//print_r($_GET['id']);exit;
+
+$result = mysqli_query($conn,$sql);
 
 if(mysqli_num_rows($result)>0){
     echo "<option value=''>Select District</option>";
     while($row=mysqli_fetch_assoc($result)){
         $district=$row["name"];
-       // echo '<option value=" ' . $district . '">' . $district . '</option>';
-       echo "<option value=".$row['name'].">". $row['name'] ."</option>";
+        $id=$row['id'];
+       echo '<option value="' . $id . '">' . $district . '</option>';
+      // echo "<option value=".$row['name'].">". $row['name'] ."</option>";
     }
 }
 ?>
